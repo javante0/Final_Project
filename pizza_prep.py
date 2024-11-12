4a204ac885728fbaf526ab39e296dccc8a462896
John Avante <johnavante7567@gmail.com>
2024-11-11 19:16:03 -0500

    Allowed time to type now changes based on number of items. Changed output to show scores for each category.

--- pizza_prep.py
+++ pizza_prep.py
@@ -11,7 +11,9 @@ order = {
 }
 
 def pizza_prep():
-    print("Type all ingredients in 10 seconds or less to receieve a perfect score.")
+    total_items = 3 + len(order['cheese']) + len(order['toppings'])
+    allowed_time = total_items * 2
+    print(f"Type all ingredients in {allowed_time} seconds or less to receieve a perfect score.")
     print("Starting in 3...")
     time.sleep(1)
     print("2...")
@@ -33,50 +35,47 @@ def pizza_prep():
     total_time = round(end-start, 2)
     
     #time score
-    if total_time <= 10:
+    if total_time <= allowed_time:
         time_score = 5
     else:
-        time_score =  5 - ((total_time - 10) / 10)
+        # Deducts 0.1 points for each second over allowed time
+        time_score =  round(5 - ((total_time - allowed_time) / 10), 2)
     
     #accuracy score
-    total_items = 3 + len(order['cheese']) + len(order['toppings'])
-
-    
     individual_score = 5/total_items
     acc_score = 0
-    c_score = 0
-    s_score = 0
-    sa_score = 0
-    ch_score = 0
-    t_score = 0
+    correct_items = 0
     
     if crust_input.lower() == order['crust'].lower():
         acc_score += individual_score
-        c_score += 1
+        correct_items += 1
 
     if size_input.lower() == order['size'].lower():
         acc_score += individual_score
-        s_score += 1
+        correct_items += 1
 
     if sauce_input.lower() == order['sauce'].lower():
         acc_score += individual_score
-        sa_score += 1
+        correct_items += 1
 
     cheese_list = [item.strip().lower() for item in cheese_input.split(",")]
     for item in cheese_list:
         if item.lower() in {item.lower() for item in order['cheese']}:
             acc_score += individual_score
-            ch_score += 1
+            correct_items += 1
 
     toppings_list = [item.strip().lower() for item in toppings_input.split(",")]
     for item in toppings_list:
         if item.lower() in {item.lower() for item in order['toppings']}:
             acc_score += individual_score
-            t_score += 1
+            correct_items += 1
     
     total_score = round((time_score + acc_score)/2, 2)
     print("\n")
     print(f"You took {total_time} second to prepare the ingredients.")
+    print(f"Time score: {time_score}/5.0")
+    print(f"You correctly prepared {correct_items} items out of {total_items}.")
+    print(f"Accuracy score: {acc_score}/5.0")
     print(f"Your total score is {total_score}/5.0")
     return total_score
 
